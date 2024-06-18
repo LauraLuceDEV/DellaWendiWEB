7ee43318fe785d5fc78c7f02090aab4cd30f97bb
Laura DEV <laura.dev.360@gmail.com>
2024-06-18 15:08:36 +0200

    form added

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Variables para el formulario
+    const garmentTypeOther = document.getElementById('garmentTypeOther');
+    const otherDescriptionGroup = document.getElementById('otherDescriptionGroup');
+
+    document.getElementsByName('garmentType').forEach(radio => {
+        radio.addEventListener('change', function() {
+            if (garmentTypeOther.checked) {
+                otherDescriptionGroup.style.display = 'block';
+            } else {
+                otherDescriptionGroup.style.display = 'none';
+            }
+        });
+    });
+
+    document.getElementById('referenceImages').addEventListener('change', function() {
+        const fileName = Array.from(this.files).map(file => file.name).join(', ');
+        this.nextElementSibling.innerText = fileName;
+    });
+
+    document.getElementById('emailForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+        displayMessage('Formulario enviado correctamente!', 'success');
+    });
+
+    function displayMessage(message, type) {
+        const messageDiv = document.getElementById('form-messages');
+        messageDiv.textContent = message;
+        messageDiv.className = type === 'error' ? 'text-danger' : 'text-success';
+    }
+
+    // Variables y funciones para el portafolio
     const portfolioRow = document.querySelector('#portfolioRow');
 
-    // Datos de ejemplo para las chaquetas
-    const chaquetas = [
+    const garabatos = [
         { 
             imagen: 'src/chaquetas-mock/chirigatos.png',
             titulo: 'Chirigatos',
@@ -41,65 +71,50 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     ];
 
-    // Espera a que el contenido de la página esté completamente cargado
     window.addEventListener("load", function() {
-        // Oculta la pantalla de carga
         document.getElementById("loader-container").style.display = "none";
-        
-        // Muestra el contenido principal de la página
-        document.body.style.overflow = "auto"; // Habilita el desplazamiento
+        document.body.style.overflow = "auto";
     });
 
-    // Función para crear una tarjeta de chaqueta
-    function crearTarjetaChaqueta(chaqueta) {
+    function crearTarjetaGarabato(garabato) {
         const card = document.createElement('div');
         card.classList.add('col-md-4');
         card.innerHTML = `
             <div class="card">
-                <img src="${chaqueta.imagen}" class="card-img-top" alt="...">
+                <img src="${garabato.imagen}" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">${chaqueta.titulo}</h5>
-                    <p class="card-text">${chaqueta.descripcion}</p>
-                    <span class="badge badge-success">${chaqueta.available}</span>
+                    <h5 class="card-title">${garabato.titulo}</h5>
+                    <p class="card-text">${garabato.descripcion}</p>
+                    <span class="badge badge-success">${garabato.available}</span>
                 </div>
             </div>
         `;
 
-        // Añadir evento clic para mostrar la ventana modal
         card.addEventListener('click', function() {
-            mostrarModal(chaqueta);
+            mostrarModal(garabato);
         });
 
         return card;
     }
 
-    // Mostrar ventana modal con detalles de la chaqueta
-    function mostrarModal(chaqueta) {
-        $('#exampleModal').modal('show'); // Mostrar ventana modal
-
-        // Actualizar contenido de la ventana modal
-        document.getElementById('modal-image').src = chaqueta.imagen;
-        document.getElementById('modal-title').textContent = chaqueta.titulo;
-        document.getElementById('modal-description').textContent = chaqueta.descripcion;
-        document.getElementById('modal-available').textContent = chaqueta.available;
+    function mostrarModal(garabato) {
+        $('#exampleModal').modal('show');
+        document.getElementById('modal-image').src = garabato.imagen;
+        document.getElementById('modal-title').textContent = garabato.titulo;
+        document.getElementById('modal-description').textContent = garabato.descripcion;
+        document.getElementById('modal-available').textContent = garabato.available;
     }
 
-    // Crear y agregar tarjetas para cada chaqueta al portafolio
-    chaquetas.forEach(chaqueta => {
-        const card = crearTarjetaChaqueta(chaqueta);
+    garabatos.forEach(garabato => {
+        const card = crearTarjetaGarabato(garabato);
         portfolioRow.appendChild(card);
     });
 
-    // Espera a que el contenido de la página esté completamente cargado
     window.addEventListener("load", function() {
-        // Oculta la pantalla de carga
         document.getElementById("loader-container").style.display = "none";
-        
-        // Muestra el contenido principal de la página
-        document.body.style.overflow = "auto"; // Habilita el desplazamiento
+        document.body.style.overflow = "auto";
     });
 
-    // Función para toggle del menú en dispositivos móviles
     const toggleNavbarButton = document.getElementById('toggleNavbar');
     const navbarCollapse = document.getElementById('navbarNav');
 
@@ -107,7 +122,6 @@ document.addEventListener('DOMContentLoaded', function() {
         navbarCollapse.classList.toggle('show');
     });
 
-    // Función para desplazamiento suave al hacer clic en los enlaces del menú
     $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(event) {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
@@ -130,21 +144,17 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    // Obtener el icono de email
-    const emailIcon = document.querySelector('.email-icon'); // Asegúrate de agregar la clase 'email-icon' al icono de email
+    const emailIcon = document.querySelector('.email-icon');
 
     emailIcon.addEventListener('click', function() {
-        $('#emailModal').modal('show'); // Mostrar ventana modal de correo electrónico
+        $('#emailModal').modal('show');
     });
 
-
-    document.addEventListener('DOMContentLoaded', () => {
-        const header = document.querySelector('header');
-        header.addEventListener('mouseover', () => {
-            header.style.backgroundImage = 'url("src/img/denim-nav-bck.png")';
-        });
-        header.addEventListener('mouseout', () => {
-            header.style.backgroundImage = 'none';
-        });
+    const header = document.querySelector('header');
+    header.addEventListener('mouseover', () => {
+        header.style.backgroundImage = 'url("src/img/denim-nav-bck.png")';
+    });
+    header.addEventListener('mouseout', () => {
+        header.style.backgroundImage = 'none';
     });
 });
